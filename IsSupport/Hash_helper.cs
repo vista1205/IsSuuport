728dbea29db04b58cdb252c82667451a70ea421e
vista35 <vista35@vtomske.ru>
2022-03-01 16:15:17 +0700

    ModelBD

--- IsSupport/Hash_helper.cs
+++ IsSupport/Hash_helper.cs
@@ -9,25 +9,57 @@ namespace IsSupport
 {
     internal class Hash_helper
     {/// <summary>
-    /// Функция для создания хеша пароля
-    /// </summary>
-    /// <param name="password"> Входные данные, которые ввел пользователь</param>
-    /// <returns>Возваращает пароль в виде хеш-суммы</returns>
+     /// Функция для создания хеша пароля
+     /// </summary>
+     /// <param name="password"> Входные данные, которые ввел пользователь</param>
+     /// <returns>Возваращает пароль в виде хеш-суммы</returns>
+        List<Employees> employees;
+
         Guid CreateHash(string password)
         {
-            byte[] bytes=Encoding.Unicode.GetBytes(password);
+            byte[] bytes = Encoding.Unicode.GetBytes(password);
             MD5CryptoServiceProvider MCP = new MD5CryptoServiceProvider();
-            byte[] byteshash=MCP.ComputeHash(bytes);
+            byte[] byteshash = MCP.ComputeHash(bytes);
             string hash = string.Empty;
-            foreach(byte b in byteshash)
+            foreach (byte b in byteshash)
             {
                 hash += string.Format("{0:x2}", b);
             }
             return new Guid(hash);
         }
-        bool CompareHash(Guid password)
+        bool CompareHash(string password)
         {
-
+            var passwordHash = CreateHash(password);
+            employees = Helper.GetIsSupportContext().Employees.ToList();
+            bool flag = false;
+            foreach (Employees employee in employees)
+            {
+                if (employee.Password == passwordHash)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            return flag; 
         }
+        int ReturnUser(string password)
+        {
+            List<Employees> employees = Helper.GetIsSupportContext().Employees.ToList();
+            int userID = 0;
+            var hashPassword=CreateHash(password);
+            if (CompareHash(password))
+            {
+                foreach(Employees employees1 in employees)
+                {
+                    if (employees1.Password == hashPassword)
+                    {
+                        userID = employees1.ID;
+                        break;
+                    }
+                }                
+            }
+            return userID;
+        } 
+
     }
 }
---
+++ IsSupport/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsSupport
+{
+    internal class Helper
+    {
+        private static IsSupportContext _context;
+         public static IsSupportContext GetIsSupportContext()
+        {
+            if (_context == null)
+            {
+                _context = new IsSupportContext();
+            }
+            return _context;
+        }
+    }
+}
